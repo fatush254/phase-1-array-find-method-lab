ea3a53228b87b6013b49ceb81161e63d73613f25
fatush254 <fgalgalo945@gmail.com>
2024-04-15 00:50:07 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+// code your solution here
+function superbowlWin(record) {
+    // Check if the record contains any win objects
+    const win = record.find(game => game.result === "W");
+  
+    // If a win object is found, return the year
+    if (win) {
+      return win.year;
+    } else {
+      // If no win object is found, return undefined
+      return undefined;
+    }
+  }
+  
+  // Export the superbowlWin function to make it available to other modules if needed
+  module.exports = superbowlWin;
